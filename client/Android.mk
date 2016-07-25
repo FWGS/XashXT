88aed89f5d0ded1fd7ac571469b0554943911da7
Night Owl <sepulkarium45@yahoo.com>
2016-07-26 03:38:21 +0500

    Remove -fpermissive flag from Android.mk.

--- client/Android.mk
+++ client/Android.mk
@@ -15,7 +15,7 @@ endif
 
 # Xash doesn't have any voice chat, so remove it from gamedll too.
 LOCAL_CFLAGS += -D_LINUX -DCLIENT_WEAPONS -DNO_VOICEGAMEMGR -Dstricmp=strcasecmp -Dstrnicmp=strncasecmp -D_snprintf=snprintf \
-	-fno-exceptions -Wextra -Wno-write-strings -fpermissive -DNX32 -DLINUX -DCLIENT_DLL=1 -fsigned-char
+	-fno-exceptions -Wextra -Wno-write-strings -DNX32 -DLINUX -DCLIENT_DLL=1 -fsigned-char
 
 LOCAL_CPPFLAGS := $(LOCAL_CFLAGS) -frtti
 
